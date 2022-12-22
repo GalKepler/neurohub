0444435fbe1d6262cf847ac14884bc8ea3c63d9a
GalKepler <galkepler@gmail.com>
2022-12-22 21:41:38 +0200

    added many to many relation between scans

--- neurohub/base_models/models/scan.py
+++ neurohub/base_models/models/scan.py
@@ -1,7 +1,10 @@
 import json
 from pathlib import Path
 
+import nibabel as nib
 from django.db import models
+
+DIMENSIONS_TO_AXES = {0: "x", 1: "y", 2: "z", 3: "t"}
 
 
 class Scan(models.Model):
@@ -15,6 +18,13 @@ class Scan(models.Model):
 
     nifti_path = models.CharField(max_length=255)
     json_path = models.CharField(max_length=255, null=True)
+    related_scans = models.ManyToManyField("self")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the Scan object."""
+        super().__init__(*args, **kwargs)
+        self.parse_json_to_attributes()
+        self.store_image_shape()
 
     def get_path_name(self):
         """Return the name of the NIfTI file."""
@@ -24,8 +34,28 @@ class Scan(models.Model):
         """Return the name of the scan."""
         return self.get_path_name()
 
-    def read_json(self):
-        """Read the JSON file."""
+    def parse_json_to_attributes(self):
+        """Parse the JSON file and set the attributes of the Scan object."""
+        if not self.json_path:
+            return
+        with open(self.json_path) as json_file:
+            json_data = json.load(json_file)
+            for key, value in json_data.items():
+                setattr(self, key, value)
 
-        with open(self.json_path) as f:
-            return json.load(f)
+    def store_image_shape(self):
+        """Store the shape of the image."""
+        dim_info = self.nifti.header.get_dim_info()
+        image_size = self.nifti.shape
+        for dim in dim_info:
+            setattr(self, f"dimension_{DIMENSIONS_TO_AXES[dim]}", image_size[dim])
+
+    @property
+    def id(self):
+        """Return the ID of the scan."""
+        return self.pk
+
+    @property
+    def nifti(self):
+        """Return the NIfTI file."""
+        return nib.load(self.nifti_path)
